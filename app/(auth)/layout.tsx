a2bd107a737582caeda5d9ce98dc73c97bd00832
Dev-mininos <amine.lbabda2@gmail.com>
2023-08-29 19:51:39 +0100

    Just adding the prisma schema

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="h-full flex items-center justify-center">{children}</div>
-  );
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="justify-center items-center flex h-full">{children}</div>;
 };
 
-export default AuthLayout;
+export default layout;
