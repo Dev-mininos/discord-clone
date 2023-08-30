b3bf5aeadc600fe3f66a974a845e0bb01214abe5
Dev-mininos <amine.lbabda2@gmail.com>
2023-08-30 12:24:06 +0100

    Navigation Side bar

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,37 @@
+"use client";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { FC } from "react";
+
+interface ActionToolTipProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+}
+
+const ActionToolTip: FC<ActionToolTipProps> = ({
+  label,
+  children,
+  side,
+  align,
+}) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default ActionToolTip;
